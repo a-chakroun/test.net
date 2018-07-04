d3cec1b1f21af9d328173aee72af01e4fc83da55
Chakroun Ahmed Amine <chakroun.a.a@gmail.com>
2018-07-04 12:48:10 +0100

    2nd

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -6,7 +6,7 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! again");
+            Console.WriteLine("Hello World! again, oh no !");
         }
     }
 }
